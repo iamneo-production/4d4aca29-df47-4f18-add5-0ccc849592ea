32f0dbb50ae1faee0fbfbeae27de53113e6c2e72
c4zuko <sabarikishore0630@gmail.com>
2022-03-25 10:42:25 +0000

    commit message

--- reactapp/src/Components/View Orders.js
+++ reactapp/src/Components/View Orders.js
@@ -1,24 +1,24 @@
 import * as ReactBootStrap from "react-bootstrap";
 
 const View=() =>{
-    const viwe =[
+    const view =[
       {orderid:"OD121211750283829010",userid:"K.Sabari",productname:"Drums",price:"100",quanity:"1"},
       {orderid:"OD121982198205115000",userid:"NaveenPrabhu",productname:"Harmonica",price:"1800",quanity:"1"},
       {orderid:"OD121211123455115000",userid:"Dinseh",productname:"Flutes",price:"2000",quanity:"1"},
       {orderid:"OD121234550705115000",userid:"Gokul",productname:"Bongos",price:"2500",quanity:"1"},
-      {orderid:"OD121211709828235000",userid:"Meghana",productname:"Piano,",price:"3500",quanity:"1"},
+      {orderid:"OD121211709828235000",userid:"Meghana",productname:"Piano",price:"3500",quanity:"1"},
       {orderid:"OD121211372837315000",userid:"Karthik",productname:"Guitar",price:"5500",quanity:"1"},
       {orderid:"OD121211234563121000",userid:"Kiruthik",productname:"Instrument Microphone",price:"1500",quanity:"1"},
       
     ]
-    const renderView = (viwe,index)=>{
+    const renderView = (view,index)=>{
       return (
         <tr className="header " key={index}>
-          <td>{ viwe.orderid}</td>
-          <td>{viwe.userid}</td>
-          <td>{viwe.productname}</td>
-          <td>{viwe.price}</td>
-          <td>{viwe.quanity}</td>
+          <td>{ view.orderid}</td>
+          <td>{view.userid}</td>
+          <td>{view.productname}</td>
+          <td>{view.price}</td>
+          <td>{view.quanity}</td>
         </tr>
       )
     }
@@ -39,7 +39,7 @@ const View=() =>{
       </tr>
     </thead>
     <tbody>
-     {viwe.map(renderView)}
+     {view.map(renderView)}
     </tbody>
   </ReactBootStrap.Table>
   
